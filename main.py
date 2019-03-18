021cc35a35abd2098672f92159845e6f69c92b22
Pedro de la Peña <pedrodelapena@uol.com.br>
2019-03-18 15:57:09 -0300

    v1.2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Token:
 		self.ttype = ttype
 		self.tvalue = tvalue
 
-class Tokenizador:
+class Tokenizer:
 	def __init__(self, origin):
 		self.origin = origin
 		self.position = 0
@@ -15,7 +15,7 @@ class Tokenizador:
 		c = "" #input placeholder | previously a number, troublemaker that made me sad and literally caused V1.0.3
 
 		if self.position >= len(self.origin): #this means we got to the end of the input and we'll have to add something there
-			token = Token(END,"finale")
+			token = Token(END,"I-END")
 			self.current=token
 
 		#going back to V0.1.X
@@ -48,6 +48,16 @@ class Tokenizador:
 			token = Token(DIV, "/") 
 			self.position += 1
 			self.current = token
+
+		elif self.origin[self.position] == '(': #we're about to give priority to something!
+			token = Token("(", "(")
+			self.position += 1
+			self.current = token
+		
+		elif self.origin[self.position] == ')': #we're about to end someone's priority!
+			token = Token(")", ")")
+			self.position += 1
+			self.current = token
 		
 		else:
 			raise Exception("Token not found")
@@ -58,58 +68,65 @@ class Parser: #token parser
 	@staticmethod 
 	def start(stg):
 		total = 0 
-		Parser.token = Tokenizador(stg) #let the fun begin	
+		Parser.token = Tokenizer(stg) #let the fun begin	
 		Parser.token.selectNext()
-	
-	def parserPri():
+
+	def parserFactor():
+		total = 0
 		if Parser.token.current.ttype == INT:
 			total = Parser.token.current.tvalue
 			Parser.token.selectNext()
-			while Parser.token.current.ttype == MULT or Parser.token.current.ttype == DIV:
-
-				if Parser.token.current.ttype == MULT: 
-					Parser.token.selectNext()
-					if Parser.token.current.ttype == INT:
-						total *= Parser.token.current.tvalue
-					else:
-						raise Exception("Error - Should have been a digit - MULT")
-
-				elif Parser.token.current.ttype == DIV:
-					Parser.token.selectNext()
-					if Parser.token.current.ttype == INT:
-						total //= Parser.token.current.tvalue
-					else:
-						raise Exception("Error - Should have been a digit - DIV")
-				else:
-					raise Exception("Error - Should have been an operator - MULT/DIV")
+		
+		elif Parser.token.current.ttype == "(":
+			Parser.token.selectNext()
+			total = Parser.parserExpression()
+			if Parser.token.current.ttype == ")":
 				Parser.token.selectNext()
-		else:
-			raise Exception("Error - Should have been a digit - MULT/DIV")
+			else:
+				raise Exception("Error - missing parenthesis )")
+
+		elif Parser.token.current.ttype == PLUS: 
+			Parser.token.selectNext()
+			total += Parser.parserFactor()
+
+		elif Parser.token.current.ttype == MINUS:
+			Parser.token.selectNext()
+			total -=  Parser.parserFactor()
+
+		return total
+
+	def parserTerm():
+		total = Parser.parserFactor()
+		while Parser.token.current.ttype == MULT or Parser.token.current.ttype == DIV:
+			if Parser.token.current.ttype == MULT: 
+				Parser.token.selectNext()
+				total *= Parser.parserFactor()
+
+			if Parser.token.current.ttype == DIV:
+				Parser.token.selectNext()
+				total //= Parser.parserFactor()
+			"""
+				else:
+					raise Exception("Error - Should have been a digit - DIV")
+			else:
+				raise Exception("Error - Should have been an operator - MULT/DIV")
+			"""
 		
 		return total
 
 
 	@staticmethod
-	def parseExpression():
-		total = Parser.parserPri() #priority 
+	def parserExpression():
+		total = Parser.parserTerm() #priority 
 		while Parser.token.current.ttype == PLUS or Parser.token.current.ttype == MINUS: 
 			if Parser.token.current.ttype == PLUS: 
 				Parser.token.selectNext()
-				if Parser.token.current.ttype == INT:
-					total += Parser.parserPri()
-				else:
-					raise Exception("Error - Should have been a digit - SUM")
+				total += Parser.parserTerm()
 
-			elif Parser.token.current.ttype == MINUS:
+			if Parser.token.current.ttype == MINUS:
 				Parser.token.selectNext()
-				if Parser.token.current.ttype == INT:
-					total -=  Parser.parserPri()
-				else:
-					raise Exception("Error - Should have been a digit - SUB")
-			else:
-				raise Exception("Error - Should have been an operator - SUM/SUB")
+				total -=  Parser.parserTerm()
 
-		print("Result = "+str(total)+"\n")
 		return total
 
 class PrePro:
@@ -131,13 +148,13 @@ PCLS = ")"	#parenthesis close
 
 def main():
 	try:
-		#now we have to make a "CLASSy" input... got it?
-		x = input("Por obséquio, insira uma operação matemática a qual vossa senhoria detenha aspiração em realizar ") + "\n"
-		x = x.replace("\\n", "\n") #previously this removed spaces and that's not good >:(
+		#done with the CLASSY joke >:(
+		x = input("Insira uma expressão ") + "\n"
+		x = x.replace("\\n", "\n") #previously this removed spaces and that's not good 
 		print("Input = "+x)
 		x = PrePro.filter(x)
 		Parser.start(x)
-		Parser.parseExpression()
+		print("Result = "+str(Parser.parserExpression()))
 	except Exception as err:
 		print(err)
 
