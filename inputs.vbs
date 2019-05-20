8c3fb665e6c58389faf4731c84e7d8252400585f
Pedro de la Peña <pedrodelapena@uol.com.br>
2019-05-20 20:06:59 -0300

    v3.0 probs not working right

--- inputs.vbs
+++ inputs.vbs
@@ -1,25 +1,13 @@
 sub main()
-dim a as integer
-dim b as integer
-dim c as boolean
-dim d as integer
-c = False
-a = 1
-b = 2
-d = 0
-print a+b
-WHILE d < 5
-    IF c THEN
-        print c 
-        c = False
-        d=d+1
-    ELSE
-        IF c = False THEN
-            print c
-            c = True
-            d=d+1
-        END IF
-    END IF
-WEND
-print a+b+d
+dim i as integer
+dim n as integer
+dim f as integer
+n = 5
+i = 2
+f = 1
+While i < n + 1
+    f = f * i
+    i = i +1
+Wend
+print f
 end sub
--- main.py
+++ main.py
@@ -335,10 +335,35 @@ class PrePro:
 		#print("Input = " + inp_stg)
 		return re.sub("'.*\n","", inp_stg) #replace substrings module
 
+class Assembler():
+	ASM_Cmds = []
+
+	def Write(stg):
+		Assembler.ASM_Cmds.append(stg)
+
+	def WriteAssembly():
+		
+		with open ("output.asm", "w+") as outfile:
+			for h in AssemblerHeader.split("\n"):
+				outfile.write(h+"\n")
+			
+			for j in Assembler.ASM_Cmds:
+				outfile.write(j+"\n")
+			
+			for f in AssemblerFooter.split("\n"):
+				outfile.write(f+"\n")
+
 class Node:
+
+	i = 0
 	def __init__(self):
 		self.value = None
 		self.children = []
+		self.id = Node.newId()
+
+	def newId():
+		Node.i += 1
+		return Node.i
 
 	def Evaluate(self):
 		pass
@@ -347,50 +372,87 @@ class BinOp(Node): #binary ops -> a(binop)b = c
 	def __init__(self, value, children):
 		self.value = value
 		self.children = children
+		self.id = Node.newId()
 
 		if len(children) != 2:
 			raise Exception("Error - two children expected, got" +self.children)			
 
 	def Evaluate(self,symb):
 		#checking if variables types match so we can go on and do ops!
-		var1 = self.children[0].Evaluate(symb)[0]
-		var2 = self.children[1].Evaluate(symb)[0]
+		var1 = self.children[0].Evaluate(symb)
+		Assembler.Write("PUSH EBX")
+		var2 = self.children[1].Evaluate(symb)
 
 		if self.value == "+":
-			return (var1 + var2, "integer")
+			#return (var1 + var2, "integer")
+			Assembler.Write("POP EAX")
+			Assembler.Write("ADD EAX, EBX")
+			Assembler.Write("MOV EBX, EAX")
 		elif self.value == "-":
-			return (var1 - var2, "integer")
+			#return (var1 - var2, "integer")
+			Assembler.Write("POP EAX")
+			Assembler.Write("SUB EAX, EBX")
+			Assembler.Write("MOV EBX, EAX")
 		elif self.value == "*":
-			return (var1 * var2, "integer")
+			#return (var1 * var2, "integer")
+			Assembler.Write("POP EAX")
+			Assembler.Write("IMUL EBX")
+			Assembler.Write("MOV EBX, EAX")
 		elif self.value == "/":
-			return (var1 // var2, "integer")
+			#return (var1 // var2, "integer")
+			Assembler.Write("POP EAX")
+			Assembler.Write("IDIV EBX")
+			Assembler.Write("MOV EBX, EAX")
 		elif self.value == "=":
-			return (var1 == var2, "boolean")
+			#return (var1 == var2, "boolean")
+			Assembler.Write("POP EAX")
+			Assembler.Write("CMP EAX, EBX")
+			Assembler.Write("CALL binop_je")
 		elif self.value == "<":
-			return (var1 < var2, "boolean")
+			#return (var1 < var2, "boolean")
+			Assembler.Write("POP EAX")
+			Assembler.Write("CMP EAX, EBX")
+			Assembler.Write("CALL binop_jl")
 		elif self.value == ">":
-			return (var1 > var2, "boolean")
+			#return (var1 > var2, "boolean")
+			Assembler.Write("POP EAX")
+			Assembler.Write("CMP EAX, EBX")
+			Assembler.Write("CALL binop_jg")
 		elif self.value == "or":
-			return (var1 or var2, "boolean")
+			#return (var1 or var2, "boolean")
+			Assembler.Write("POP EAX")
+			Assembler.Write("OR EAX, EBX")
+			Assembler.Write("MOV EBX, EAX")
 		elif self.value == "and":
-			return (var1 and var2, "boolean")
+			#return (var1 and var2, "boolean")
+			Assembler.Write("POP EAX")
+			Assembler.Write("AND EAX, EBX")
+			Assembler.Write("MOV EBX, EAX")
 
 class UnOp(Node): #unary ops -> -(a) = -a | -(-a) = a
 	def __init__(self, value, children):
 		self.value = value
 		self.children = children
+		self.id = Node.newId()
 	
 	def Evaluate(self, symb):
 		if self.value == "+":
-			return self.children[0].Evaluate(symb)
+			#return self.children[0].Evaluate(symb)
+			Assembler.Write("MOV EAX, 1")
+			Assembler.Write("IMUL EBX")
+			Assembler.Write("MOV EBX, EAX")
 		elif self.value == "-":
-			var = self.children[0].Evaluate(symb)
-			var = (var[0]*-1,var[1])
-			return var
+			#var = self.children[0].Evaluate(symb)
+			#var = (var[0]*-1,var[1])
+			#return var
+			Assembler.Write("MOV EAX, -1")
+			Assembler.Write("IMUL EBX")
+			Assembler.Write("MOV EBX, EAX")
 		elif self.value == "not":
-			var = self.children[0].Evaluate(symb)
-			var = (not var[0],var[1])		
-			return var
+			#var = self.children[0].Evaluate(symb)
+			#var = (not var[0],var[1])		
+			#return var
+			Assembler.Write("NEG EBX")
 		else:
 			raise Exception("Error - undefined UnOp: "+ str(self.value))
 
@@ -398,14 +460,17 @@ class IntVal(Node): #gets and returns int
 	def __init__(self, value, children): 
 		self.value = value
 		self.children = children	
+		self.id = Node.newId()
 	
 	def Evaluate(self, symb):
-		return (self.value,"integer")
+		#return (self.value,"integer")
+		Assembler.Write("MOV EBX, "+str(self.value))
 
 class NoOp(Node): #dummy - nothing to do here
 	def __init__(self, value, children):
 		self.value = value
 		self.children = children	
+		self.id =  Node.newId()
 
 	def Evaluate(self,symb):
 		pass
@@ -414,51 +479,75 @@ class Assignment(Node): #sets value to given variable - a = K
 	def __init__(self, value, children):
 		self.value = value
 		self.children = children
+		self.id = Node.newId()
 	
 	def Evaluate(self, symb):
 		symb.setter(self.children[0].value, self.children[1].Evaluate(symb))
+
+		Assembler.Write("MOV [EBP-"+str(symb.getter(self.children[0].value)[2])+"], EBX")
 
 
 class Identifier(Node):
 	def __init__(self, value, children):
 		self.value = value
 		self.children = children
+		self.id = Node.newId()
 	
 	def Evaluate(self, symb):
-		return symb.getter(self.value)
+		#return symb.getter(self.value)
+		Assembler.Write("MOV EBX, [EBP-"+str(symb.getter(self.value)[2])+"]")
 
 class Statements(Node): #statement in statements
 	def __init__(self, value, children):
 		self.value = value
 		self.children = children
+		self.id = Node.newId()
 	
 	def Evaluate(self, symb):
 		for i in self.children:
-			#idk why this doesn't work... I don't know what to do anymore
 			i.Evaluate(symb)
 
 class Print(Node): 
 	def __init__(self, value, children):
 		self.value = value
 		self.children = children
+		self.id = Node.newId()
 	
 	def Evaluate(self,symb):
-		print(self.children[0].Evaluate(symb)[0])
+		self.children[0].Evaluate(symb)
+		Assembler.Write("PUSH EBX")
+		Assembler.Write("CALL print")
+		Assembler.Write("POP EBX")
 
 class WhileOp(Node):
 	def __init__(self, value, children):
 		self.value = value
 		self.children = children
+		self.id = Node.newId()
 
 	def Evaluate(self,symb):
-		while self.children[0].Evaluate(symb)[0]:
-			self.children[1].Evaluate(symb)
+		Assembler.Write("LOOP_"+str(self.id)+":")
+
+		self.children[0].Evaluate(symb)
+
+		Assembler.Write("CMP EBX, False")
+		Assembler.Write("JE EXIT_"+str(self.id))
+
+		self.children[1].Evaluate(symb)
+
+		Assembler.Write("JMP LOOP_"+str(self.id))
+		Assembler.Write("EXIT_"+str(self.id)+":")
 
 class IfOp(Node):
 	def __init__(self, children):
 		self.children = children
+		self.id = Node.newId()
 	
 	def Evaluate(self,symb):
+
+		Assembler.Write("CMP EBX, True")
+		Assembler.Write("JE if_"+str(self.id))
+
 		if self.children[0].Evaluate(symb)[1] == 'boolean':
 			self.children[1].Evaluate(symb)
 		else:
@@ -467,31 +556,39 @@ class IfOp(Node):
 			else:
 				pass
 
+		Assembler.Write("JMP EXIT_"+str(self.id))
+		Assembler.Write("if_"+str(self.id))
+		Assembler.Write("EXIT_"+str(self.id))
+
 class InputOp(Node):
 	def __init__(self, value,children):
 		self.value = value
 		self.children = children
+		self.id = Node.newId()
 	
 	def Evaluate(self,symb):
-		return (int(input()),"integer")
+		#return (int(input()),"integer")
+		pass
 
 class NodeType(Node):
 	def __init__(self, value):
 		self.value = value
-
-		temptrylist = ["integer","boolean"]
-		if value.lower() not in temptrylist:
-			raise Exception("Error - unexpected type " + str(self.value))
+		self.id = Node.newId()
+		#temptrylist = ["integer","boolean"]
+		#if value.lower() not in temptrylist:
+		#	raise Exception("Error - unexpected type " + str(self.value))
 			
 	def Evaluate(self, symb):
-		if self.value == "integer":
-			return (self.value,0)
-		elif self.value == "boolean":
-			return (self.value, False)
+		#if self.value == "integer":
+		#	return (self.value,0)
+		#elif self.value == "boolean":
+		#	return (self.value, False)
+		pass
 
 class SymbolTable:
 	def __init__(self):
 		self.varDict = {}
+		self.shift = 0
 
 	def getter(self, var): #returns value for variable
 		if var in self.varDict.keys():
@@ -502,26 +599,31 @@ class SymbolTable:
 	def setter(self, var, value): #assigns value to variable
 		if var not in self.varDict:
 			raise Exception("Error - undeclared variable")		
-		self.varDict[var] = value
+		self.varDict[var][0] = value
 	
-	def declarator(self, var, value):
+	def declarator(self, var, value, tp):
 		if var in self.varDict:
 			raise Exception("Error - duplicate variable")
-		self.varDict[var] = value
+		self.shift += 4
+		self.varDict[var] = [None, tp, self.shift]
 
 class VarDec(Node):
 	def __init__(self, children):
 		self.children = children
+		self.id = Node.newId()
 	
 	def Evaluate(self, symb):
-		symb.declarator(self.children[0].value, self.children[1].Evaluate(symb))
+		symb.declarator(self.children[0].value, None, self.children[1].Evaluate(symb))
+		Assembler.Write("PUSH DWORD 0")
 
 class BoolValue(Node):
 	def __init__(self, value):
 		self.value = value
+		self.id = Node.newId()
 	
 	def Evaluate(self,symb):
-		return (self.value,"boolean")
+		#return (self.value,"boolean")
+		Assembler.Write("MOV EBX, "+str(self.value))
 
 
 #Tokens -- THIS IS A MESS AND I REGRET DOING THIS FOR REAL
@@ -551,24 +653,110 @@ ELSE = "ELSE"
 RWL = ["BEGIN", "END", "PRINT", "IF", "THEN", "ELSE", "OR", "AND", "WHILE", "WEND", "EOF",
 		"INPUT", "NOT", "DIM", "INTEGER", "BOOLEAN", "TRUE", "FALSE", "AS", "MAIN", "SUB"] 
 
+AssemblerHeader = """; constantes
+SYS_EXIT equ 1
+SYS_READ equ 3
+SYS_WRITE equ 4
+STDIN equ 0
+STDOUT equ 1
+True equ 1
+False equ 0
+
+segment . data
+
+segment . bss ; variaveis
+  res RESB 1
+
+section .text
+  global_start
+
+print:  ; subrotina print
+  PUSH EBP ; guarda o base pointer
+  MOV EBP, ESP ; estabelece um novo base pointer
+
+  MOV EAX, [EBP+8] ; 1 argumento antes do RET e EBP
+  XOR ESI, ESI
+
+print_dec: ; empilha todos os digitos
+  MOV EDX, 0
+  MOV EBX, 0x000A
+  DIV EBX
+  ADD EDX, '0'
+  PUSH EDX
+  INC ESI ; contador de digitos
+  CMP EAX, 0
+  JZ print_next ; quando acabar pula
+  JMP print_dec
+
+print_next:
+  CMP ESI, 0
+  JZ print_exit ; quando acabar de imprimir
+  DEC ESI
+
+  MOV EAX, SYS_WRITE
+  MOV EBX, STDOUT
+
+  POP ECX
+  MOV [res], ECX
+  MOV ECX, res
+
+  MOV EDX, 1
+  INT 0x80
+  JMP print_next
+
+print_exit:
+  POP EBP
+  RET
+
+; subrotinas if/while
+binop_je:
+  JE binop_true
+  JMP binop_false
+
+binop_jg:
+  JG binop_true
+  JMP binop_false
+
+binop_jl:
+  JL binop_true
+  JMP binop_false
+
+binop_false:
+  MOV EBX, False  
+  JMP binop_exit
+binop_true:
+  MOV EBX, True
+binop_exit:
+  RET
+"""
+
+AssemblerFooter = """
+; interrupcao de saida
+POP EBP
+MOV EAX, 1
+INT 0 x80
+"""
+
+
 def main():
 	
 	symb = SymbolTable()
-	#try:
-	inpFile = "inputs.vbs"
-	#	inpFile = sys.argv[1]
-	#except IndexError:
-	#	print("failed to find file")
-	#	sys.exit(1)
+	try:
+	#inpFile = "inputs.vbs"
+		inpFile = sys.argv[1]
+	except IndexError:
+		print("failed to find file")
+		sys.exit(1)
 
 	with open(inpFile, "r") as file:
 		inp = file.read() +"\n"
-	#try:
-	inp = inp.replace("\\n", "\n") 
-	out = Parser.run(inp)
-	out.Evaluate(symb)
-	#except Exception as err:
-	#	print(err)
+	try:
+		inp = inp.replace("\\n", "\n") 
+		out = Parser.run(inp)
+		out.Evaluate(symb)
+		Assembler.WriteAssembly()
+	except Exception as err:
+		print(err)
 
 if __name__== "__main__":
     main()
